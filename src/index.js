cbf8185e1ebc07093ff52b28749369f8a6d9bdff
Arthur Jenck <arthurjenckdev@gmail.com>
2024-09-24 17:47:50 +0200

    feat: added index.html for Github Pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-/*
-     eslint-disable react/jsx-filename-extension
-*/
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -9,10 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
